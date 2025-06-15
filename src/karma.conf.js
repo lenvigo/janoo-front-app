9b50b1d77e88d2142d6202f27e450e3c0df188a8
lenvigo <lenvigo2308@gmail.com>
2025-06-15 14:52:43 +0200

    test: add ChromeHeadlessCI custom launcher to Karma config

--- src/karma.conf.js
+++ src/karma.conf.js
@@ -6,6 +6,18 @@ module.exports = function (config) {
       require("karma-jasmine-html-reporter"),
       require("@angular-devkit/build-angular/plugins/karma"),
     ],
+    browsers: ["ChromeHeadlessCI"],
+    customLaunchers: {
+      ChromeHeadlessCI: {
+        base: "ChromeHeadless",
+        flags: [
+          "--no-sandbox",
+          "--disable-setuid-sandbox",
+          "--disable-gpu",
+          "--remote-debugging-port=9222",
+        ],
+      },
+    },
     client: { clearContext: false },
     reporters: ["progress", "kjhtml"],
     browsers: ["ChromeHeadless"],
